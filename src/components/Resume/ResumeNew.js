d7a6da6f2485c3cebf80dc4cf11f5f7d36d432d7
Samsheeray Allam A <102540470+Samsheer1027@users.noreply.github.com>
2023-01-24 19:36:13 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "src/Assets/My_Profile.pdf";
+  "http://localhost:3000/static/media/My_Profile.cb4a88bb02f91d5cf5e8.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
